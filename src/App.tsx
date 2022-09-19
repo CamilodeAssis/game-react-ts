610ba51c68475ca7fd6798c3d668f8b34b20ff6f
Camilo de Assis <camilo.kotecki10@gmail.com>
2022-09-19 18:15:23 -0300

    update template

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 
 
-function App() {
-
-
+const App = () => {
   return (
-    <div>
-      
+    //container
+    <div className='w-full max-w-3xl m-auto flex py-12 flex-col md:flex-row ' >
+      {/*info esquerda */}
+      <div className="flex flex-col w-auto items-center  mb-12 md:items-start " >
+        <a className="block" href="https://github.com/CamilodeAssis" target="_blank">
+          <span>Criado por Camilo de Assis</span>
+        </a>
+
+        <div className="w-full my-3 flex justify-around text-center md:block  ">
+          ...
+        </div>
+
+        <button className="bg-redValo w-48 md:w-full text-beigeValo">Reset</button>
+
+      </div>
+
+      {/*grid direita */}
+      <div className="flex-1 flex justify-center mx-5 md:justify-end">
+        ...
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,13 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        redValo: '#FF4655',
+        beigeValo: '#ECE8E1',
+        
+      }
+    },
   },
   plugins: [],
 }
